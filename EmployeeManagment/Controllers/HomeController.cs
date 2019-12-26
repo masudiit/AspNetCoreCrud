3559567ff1399c80697b85da75895a6553dc2fad
masudiit <masud.iit@gmail.com>
2019-12-26 12:32:56 -0600

    Edit View

--- EmployeeManagment/Controllers/HomeController.cs
+++ EmployeeManagment/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ using System.IO;
 
 namespace EmployeeManagment.Controllers
 {
-    public class HomeController : Controller 
+    public class HomeController : Controller
     {
         private IEmployeeRepository _employeeRepository;
         private IHostingEnvironment hostingEnvironment;
@@ -29,7 +29,7 @@ namespace EmployeeManagment.Controllers
 
         public ViewResult Details(int id)
         {
-            
+
             HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
             {
                 Employee = _employeeRepository.GetEmployee(id),
@@ -46,32 +46,77 @@ namespace EmployeeManagment.Controllers
             return View();
         }
 
+        [HttpGet]
+        public ViewResult Edit(int id)
+        {
+            Employee employee = _employeeRepository.GetEmployee(id);
+            EmployeeEditViewModel employeeEditViewModel = new EmployeeEditViewModel
+            {
+                Id = employee.Id,
+                Name = employee.Name,
+                Email = employee.Email,
+                Department = employee.Department,
+                ExistingPhotoPath = employee.PhotoPath
+            };
+            return View(employeeEditViewModel);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(EmployeeEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                Employee employee = _employeeRepository.GetEmployee(model.Id);
+                employee.Name = model.Name;
+                employee.Email = model.Email;
+                employee.Department = model.Department;
+                
+
+                if (model.Photo !=null) {
+
+                    if(model.ExistingPhotoPath != null)
+                    {
+                        string filePath = Path.Combine(hostingEnvironment.WebRootPath, 
+                            "images",model.ExistingPhotoPath);
+                        System.IO.File.Delete(filePath);
+                    }
+                    employee.PhotoPath = ProcessUploadFile(model);
+                }
+               
+                _employeeRepository.Update(employee);
+                return RedirectToAction("index");
+            }
+
+            return View();
+        }
+
+        private string ProcessUploadFile(EmployeeCreateViewModel model)
+        {
+            string uniqueFileName = null;
+            if (model.Photo != null)
+            {
+
+                string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+                string filePath = System.IO.Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new System.IO.FileStream(filePath, FileMode.Create))
+                {
+                    model.Photo.CopyTo(fileStream);
+                }
+            }
+
+            return uniqueFileName;
+        }
+
         [HttpPost]
         public IActionResult Create(EmployeeCreateViewModel model)
         {
             if (ModelState.IsValid)
             {
-                string uniqueFileName = null;
-                //if(model.Photo != null && model.Photo.Count >0)
-                //{foreach (IFormFile photo in model.Photo)
-                //    {
-                //        string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
-                //        uniqueFileName = Guid.NewGuid().ToString() + "_" + photo.FileName;
-                //        string filePath = System.IO.Path.Combine(uploadsFolder, uniqueFileName);
-                //        photo.CopyTo(new System.IO.FileStream(filePath, FileMode.Create));
-                //    }
-                //}
-
-
-                if (model.Photo != null )
-                {
-                   
-                        string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
-                        uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
-                        string filePath = System.IO.Path.Combine(uploadsFolder, uniqueFileName);
-                        model.Photo.CopyTo(new System.IO.FileStream(filePath, FileMode.Create));
-                  
-                }
+                
+                string uniqueFileName = ProcessUploadFile(model);
                 Employee newEmployee = new Employee
                 {
                     Name = model.Name,
