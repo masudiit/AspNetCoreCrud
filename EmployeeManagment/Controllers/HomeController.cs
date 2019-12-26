311c77e694a186bb6c7daa0fa9f1228ca260c53d
masudiit <masud.iit@gmail.com>
2019-12-26 10:39:38 -0600

    pHOTO

--- EmployeeManagment/Controllers/HomeController.cs
+++ EmployeeManagment/Controllers/HomeController.cs
@@ -1,20 +1,23 @@
 ﻿using EmployeeManagment.Models;
 using EmployeeManagment.ViewModels;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace EmployeeManagment.Controllers
 {
     public class HomeController : Controller 
     {
         private IEmployeeRepository _employeeRepository;
+        private IHostingEnvironment hostingEnvironment;
 
-        public HomeController(IEmployeeRepository employeeRepository)
+        public HomeController(IEmployeeRepository employeeRepository,
+                              IHostingEnvironment hostingEnvironment)
         {
             _employeeRepository = employeeRepository;
+            this.hostingEnvironment = hostingEnvironment;
         }
 
         public ViewResult Index()
@@ -26,22 +29,7 @@ namespace EmployeeManagment.Controllers
 
         public ViewResult Details(int id)
         {
-            //Employee model = _employeeRepository.GetEmployee(1);
-            //ViewData["Employee"] = model;
-            //ViewData["PageTitle"] = "Employee Details";
-            //return View();
-
-            //Employee model = _employeeRepository.GetEmployee(1);
-            //ViewBag.Employee = model;
-            //ViewBag.PageTitle = "Employee Details";
-            //return View();
-
-
-            //Employee model = _employeeRepository.GetEmployee(1);
-            //ViewBag.PageTitle = "Employee Details";
-            //return View(model);
-
-
+            
             HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
             {
                 Employee = _employeeRepository.GetEmployee(id),
@@ -59,17 +47,56 @@ namespace EmployeeManagment.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Employee employee)
+        public IActionResult Create(EmployeeCreateViewModel model)
         {
             if (ModelState.IsValid)
             {
-                Employee newEmployee = _employeeRepository.Add(employee);
+                string uniqueFileName = null;
+                //if(model.Photo != null && model.Photo.Count >0)
+                //{foreach (IFormFile photo in model.Photo)
+                //    {
+                //        string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
+                //        uniqueFileName = Guid.NewGuid().ToString() + "_" + photo.FileName;
+                //        string filePath = System.IO.Path.Combine(uploadsFolder, uniqueFileName);
+                //        photo.CopyTo(new System.IO.FileStream(filePath, FileMode.Create));
+                //    }
+                //}
+
+
+                if (model.Photo != null )
+                {
+                   
+                        string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
+                        uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+                        string filePath = System.IO.Path.Combine(uploadsFolder, uniqueFileName);
+                        model.Photo.CopyTo(new System.IO.FileStream(filePath, FileMode.Create));
+                  
+                }
+                Employee newEmployee = new Employee
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    Department = model.Department,
+                    PhotoPath = uniqueFileName
+                };
+                _employeeRepository.Add(newEmployee);
                 return RedirectToAction("details", new { id = newEmployee.Id });
             }
 
             return View();
-                
         }
+
+        //[HttpPost]
+        //public IActionResult Create(Employee employee)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        Employee newEmployee = _employeeRepository.Add(employee);
+        //        return RedirectToAction("details", new { id = newEmployee.Id });
+        //    }
+
+        //    return View();               
+        //}
 
     }
 }
